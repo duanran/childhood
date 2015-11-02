8d3870d93d45efaeeaf237746c85852ff4ee3448
DuanRan <duanr@polysaas.com>
2015-11-02 09:53:58 +0800

    添加baserequest

---
+++ Childhood/Childhood/Common/CHMBaseRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CHMBaseRequest.swift
+//  Childhood
+//
+//  Created by duanran on 15/10/30.
+//  Copyright © 2015年 duanran. All rights reserved.
+//
+
+import UIKit
+
+class CHMBaseRequest: NSObject {
+
+}
